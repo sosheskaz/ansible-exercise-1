77f31f004fb79557d7685dcd2f5f6df4ec31ef8a
Eric Miller <emiller@cleo.com>
2017-07-03 13:01:29 -0500

    Added some functionality to secretsgen.rb, and parameterized  aws_ssh_key.

--- ansible/secretsgen.rb
+++ ansible/secretsgen.rb
@@ -7,8 +7,12 @@ puts 'Enter your AWS secret key.'
 aws_secret = gets
 puts 'Enter the VPC ID to run under.'
 vpc = gets
+puts 'Enter the absolute path on the controller machine (defaults to localhost) to your AWS private SSH key (ex: ~/example.pem).'
+ssh_key = gets
 puts 'Enter your email.'
 email = gets
+puts 'Enter any additional parameters you want to use for ansible vault (ex: "--ask-vault-pass" or "--vault-password-file ~/mypasswordfile")'
+extra_args = gets
 
 dir = File.dirname(__FILE__)
 def pw()
@@ -19,10 +23,11 @@ open("#{dir}/vars/secrets.yml", 'w') { |f|
   f.puts "aws_access_key: #{aws_access}"
   f.puts "aws_secret_key: #{aws_secret}"
   f.puts "aws_vpc_id: #{vpc}"
+  f.puts "aws_ssh_key: #{ssh_key}"
   f.puts "wp_admin_email: #{email}"
   f.puts "wp_admin_pass: #{pw()}"
   f.puts "mysql_root_password: #{pw()}"
   f.puts "wp_db_password: #{pw()}"
 }
 
-`ansible-vault encrypt #{dir}/vars/secrets.yml`
+`ansible-vault encrypt #{dir}/vars/secrets.yml #{extra_args}`
