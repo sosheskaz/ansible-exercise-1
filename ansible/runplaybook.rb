dbcc5ed1727bd9512291c34f4d7a83abe4cef4dd
Eric Miller <emiller@cleo.com>
2017-07-03 12:00:30 -0500

    Added role dependencies, cleaned up some miscellaneous things.
    - Made runplaybook.rb more functional.
    - Switched more names to lowercase.
    - Standardized to single quotes where it makes sense.
    - Removed some unnecessary steps from MySQL role.
    - Removed restart nginx handler, just restart every time to avoid double restarts.
    - Parameterized wp_cli

--- ansible/runplaybook.rb
+++ ansible/runplaybook.rb
@@ -5,8 +5,13 @@ require 'open3'
 dir = File.dirname(__FILE__)
 
 puts "/usr/bin/env ansible-playbook -i #{dir}/hosts -v #{dir}/main.yml"
-Open3.popen2e("/usr/bin/env ansible-playbook -i #{dir}/hosts #{dir}/main.yml") do |stdin, stdout_and_stderr, wait_thr|
-  while line=stdout_and_stderr.gets do
-    puts line
+
+open("#{dir}/playbook.log", 'w') { |f|
+  Open3.popen2e("/usr/bin/env ansible-playbook -i #{dir}/hosts #{dir}/main.yml") do |stdin, stdout_and_stderr, wait_thr|
+    while line=stdout_and_stderr.gets do
+      puts line
+      f.puts line
+      f.flush
+    end
   end
-end
+}
